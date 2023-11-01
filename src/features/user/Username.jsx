ce0f9b4821789f4b9bc0d04f44334e4c1a26458e
shubhamgoyalll <goyalshubham2521@gmail.com>
2023-11-01 13:30:17 +0530

    Tailwind Flex Box

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Shubham</div>;
+  return <div className="hidden text-sm font-semibold md:block">Shubham</div>;
 }
 
 export default Username;
